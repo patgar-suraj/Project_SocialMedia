66047b3d6c86f35131fdd7e3388ccbecf441b301
patgar-suraj <surajrp7012@gmail.com>
2025-12-30 18:58:34 +0530

    fix: update API base URL from localhost:5000 to localhost:3000 in architecture and code examples; refactor NavBar layout for better responsiveness

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -46,10 +46,10 @@ const NavBar = () => {
 
   return (
     <nav className="bg-[#1a1f2e] border-b border-[#3f46e1] p-4">
-      <div className="max-w-7xl mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold text-white">ImageCaption AI</h1>
+      <div className="max-w-7xl mx-auto flex flex-col md:flex-row gap-2 justify-between items-start">
+        <h1 className="w-fit text-2xl font-bold text-white">ImageCaption AI</h1>
 
-        <div className="flex items-center gap-4">
+        <div className="w-full md:w-fit flex gap-5 items-center justify-between md:justify-center">
           {user ? (
             <>
               <span className="text-white font-semibold">
@@ -57,7 +57,7 @@ const NavBar = () => {
               </span>
               <button
                 onClick={handleLogout}
-                className="bg-red-600 hover:bg-red-700 text-white font-semibold px-4 py-2 rounded transition"
+                className="bg-red-600 hover:bg-red-700 text-white font-semibold px-4 py-1 md:py-2 rounded transition"
               >
                 Logout
               </button>
@@ -79,6 +79,8 @@ const NavBar = () => {
             </>
           )}
         </div>
+
+        
       </div>
     </nav>
   );
